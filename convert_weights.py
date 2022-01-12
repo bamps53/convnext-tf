0675201f5680e2fc62a2fd698e5c4b8e10fa51d8
bamps53 <daisuke0530@gmail.com>
2022-01-12 14:26:36 +0000

    init

---
+++ convert_weights.py
@@ -0,0 +1,116 @@
+import os
+import timm
+import torch
+import numpy as np
+import cv2
+import tensorflow as tf
+# from tensorflow.compat.v1 import ConfigProto
+# from tensorflow.compat.v1 import InteractiveSession
+
+# config = ConfigProto()
+# config.gpu_options.allow_growth = True
+# session = InteractiveSession(config=config)
+
+from models.convnext import * # just to register model
+from models.convnext_tf import create_model
+
+model_urls = [
+    'https://dl.fbaipublicfiles.com/convnext/convnext_tiny_1k_224_ema.pth',
+    'https://dl.fbaipublicfiles.com/convnext/convnext_base_22k_1k_224.pth',
+    'https://dl.fbaipublicfiles.com/convnext/convnext_base_22k_1k_384.pth',
+    'https://dl.fbaipublicfiles.com/convnext/convnext_large_22k_1k_224.pth',
+    'https://dl.fbaipublicfiles.com/convnext/convnext_large_22k_1k_384.pth',
+    'https://dl.fbaipublicfiles.com/convnext/convnext_xlarge_22k_1k_224_ema.pth',
+    'https://dl.fbaipublicfiles.com/convnext/convnext_xlarge_22k_1k_384_ema.pth',
+]
+
+def load_test_image(image_size=(224, 224)):
+    from skimage.data import chelsea
+    img = chelsea()  # Chelsea the cat
+    img = cv2.resize(img, image_size)
+    img = img / 255
+    img = (img - np.array([0.485, 0.456, 0.406])) / np.array([0.229, 0.224, 0.225])
+    return img
+
+def load_from_url(model, url):
+    checkpoint = torch.hub.load_state_dict_from_url(url=url, map_location="cpu", check_hash=True)
+    model.load_state_dict(checkpoint["model"])
+    return model
+
+def get_pt_state_dict(pt_model):
+    pt_state_dict = {}
+    for n, p in pt_model.named_parameters():
+        pt_state_dict[n] = p.detach().numpy()
+    return pt_state_dict
+
+
+def get_tf_state_dict(tf_model):
+    tf_state_dict = {}
+    for layer in tf_model.layers:
+        for var in layer.weights:
+            if ('dense' in var.name) and ('kernel' in var.name):
+                w = tf.transpose(var, [1, 0]).numpy()
+            elif ('depthwise_conv2d' in var.name) and ('kernel' in var.name):
+                w = tf.transpose(var, [2, 3, 0, 1]).numpy()
+            elif ('conv2d' in var.name) and ('kernel' in var.name):
+                w = tf.transpose(var, [3, 2, 0, 1]).numpy()
+            else:
+                w = var.numpy()
+            tf_state_dict[var.name] = w
+    return tf_state_dict
+
+def convert_weights(tf_model, pt_state_dict, key_map):
+    for layer in tf_model.layers:
+        for var in layer.weights:
+            pt_key = key_map[var.name]
+            pt_w = pt_state_dict[pt_key]
+            if ('dense' in var.name) and ('kernel' in var.name):
+                tf_w = np.transpose(pt_w, [1, 0])
+            elif ('depthwise_conv2d' in var.name) and ('kernel' in var.name):
+                tf_w = np.transpose(pt_w, [2, 3, 0, 1])
+            elif ('conv2d' in var.name) and ('kernel' in var.name):
+                tf_w = np.transpose(pt_w, [2, 3, 1, 0])
+            else:
+                tf_w = pt_w
+            var.assign(tf_w)
+
+def test_models(pt_model, tf_model):
+    img = load_test_image()
+    pt_x = torch.tensor(img[None,]).permute(0, 3, 1, 2).to(torch.float32)
+    with torch.no_grad():
+        pt_y = torch.softmax(pt_model(pt_x), -1).numpy()
+
+    tf_x = tf.convert_to_tensor(img[None,])
+    tf_y = tf.nn.softmax(tf_model(tf_x)).numpy()
+    np.testing.assert_allclose(pt_y, tf_y, rtol=1e-5)
+    
+
+def main():
+    os.makedirs('weights', exist_ok=True)
+    for model_url in model_urls:
+        file_name = os.path.basename(model_url)
+        model_name = '_'.join(file_name.split('_')[:2])
+        save_name = file_name.replace("pth", "h5")
+        if os.path.exists(f'weights/{save_name}'):
+            continue
+
+        pt_model = timm.create_model(model_name)
+        pt_model = load_from_url(pt_model, model_url)
+        pt_model.eval()
+        pt_state_dict = get_pt_state_dict(pt_model)
+
+        tf_model = create_model(model_name)
+        tf_state_dict = get_tf_state_dict(tf_model)
+
+        key_map = {t:p for t, p in zip(tf_state_dict.keys(), pt_state_dict.keys())}
+        convert_weights(tf_model, pt_state_dict, key_map)
+
+        test_models(pt_model, tf_model)
+        print(f'successfully converted {model_name}!')
+
+        
+        
+        tf_model.save_weights(f'weights/{save_name}')
+        
+if __name__ == '__main__':
+    main()
---
+++ models/convnext_tf.py
@@ -0,0 +1,240 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.layers import (
+    Layer, DepthwiseConv2D, LayerNormalization,
+    Dense, Conv2D, GlobalAveragePooling2D
+)
+
+
+def drop_path(inputs, drop_prob, is_training):
+    # borrowed from https://github.com/rishigami/Swin-Transformer-TF/blob/main/swintransformer/model.py
+    if (not is_training) or (drop_prob == 0.):
+        return inputs
+
+    # Compute keep_prob
+    keep_prob = 1.0 - drop_prob
+
+    # Compute drop_connect tensor
+    random_tensor = keep_prob
+    shape = (tf.shape(inputs)[0],) + (1,) * \
+        (len(tf.shape(inputs)) - 1)
+    random_tensor += tf.random.uniform(shape, dtype=inputs.dtype)
+    binary_tensor = tf.floor(random_tensor)
+    output = tf.math.divide(inputs, keep_prob) * binary_tensor
+    return output
+
+class DropPath(tf.keras.layers.Layer):
+    # borrowed from https://github.com/rishigami/Swin-Transformer-TF/blob/main/swintransformer/model.py
+    def __init__(self, drop_prob=None):
+        super().__init__()
+        self.drop_prob = drop_prob
+
+    def call(self, x, training=None):
+        return drop_path(x, self.drop_prob, training)
+
+class Block(Layer):
+    r""" ConvNeXt Block. There are two equivalent implementations:
+    (1) DwConv -> LayerNorm (channels_first) -> 1x1 Conv -> GELU -> 1x1 Conv; all in (N, C, H, W)
+    (2) DwConv -> Permute to (N, H, W, C); LayerNorm (channels_last) -> Linear -> GELU -> Linear; Permute back
+    We use (2) as we find it slightly faster in PyTorch
+    
+    Args:
+        dim (int): Number of input channels.
+        drop_path (float): Stochastic depth rate. Default: 0.0
+        layer_scale_init_value (float): Init value for Layer Scale. Default: 1e-6.
+    """
+    def __init__(self, dim, drop_path=0., layer_scale_init_value=1e-6, prefix=''):
+        super().__init__()
+        self.dwconv = DepthwiseConv2D(kernel_size=7, padding='same') # depthwise conv
+        self.norm = LayerNormalization(epsilon=1e-6)
+        self.pwconv1 = Dense(4 * dim) # pointwise/1x1 convs, implemented with linear layers
+        self.act = tf.keras.activations.gelu
+        self.pwconv2 = Dense(dim)
+        self.drop_path = DropPath(drop_path)
+        self.dim = dim
+        self.layer_scale_init_value = layer_scale_init_value
+        self.prefix = prefix
+
+    def build(self, input_shape):
+        self.gamma = tf.Variable(
+            initial_value=self.layer_scale_init_value * tf.ones((self.dim)),
+            trainable=True,
+            name=f'{self.prefix}/gamma')
+        self.built = True
+
+    def call(self, x):
+        input = x
+        x = self.dwconv(x)
+        # x = x.permute(0, 2, 3, 1) # (N, C, H, W) -> (N, H, W, C)
+        x = self.norm(x)
+        x = self.pwconv1(x)
+        x = self.act(x)
+        x = self.pwconv2(x)
+        if self.gamma is not None:
+            x = self.gamma * x
+        # x = x.permute(0, 3, 1, 2) # (N, H, W, C) -> (N, C, H, W)
+
+        x = input + self.drop_path(x)
+        return x
+
+class ConvNeXt(tf.keras.Model):
+    r""" ConvNeXt
+        A Tensorflow keras impl of : `A ConvNet for the 2020s`  -
+          https://arxiv.org/pdf/2201.03545.pdf
+
+    Args:
+        num_classes (int): Number of classes for classification head. Default: 1000
+        depths (tuple(int)): Number of blocks at each stage. Default: [3, 3, 9, 3]
+        dims (int): Feature dimension at each stage. Default: [96, 192, 384, 768]
+        include_top (bool): whether to add head or just use it as feature extractor. Default: True
+        drop_path_rate (float): Stochastic depth rate. Default: 0.
+        layer_scale_init_value (float): Init value for Layer Scale. Default: 1e-6.
+        head_init_scale (float): Init scaling value for classifier weights and biases. Default: 1.
+    """
+    def __init__(self, num_classes=1000, 
+                 depths=[3, 3, 9, 3], dims=[96, 192, 384, 768], include_top=True,
+                 drop_path_rate=0., layer_scale_init_value=1e-6, head_init_scale=1.,
+                 ):
+        super().__init__()
+        self.include_top = include_top
+        self.downsample_layers = [] # stem and 3 intermediate downsampling conv layers
+        stem = tf.keras.Sequential([
+            Conv2D(dims[0], kernel_size=4, strides=4, padding='same'),
+            LayerNormalization(epsilon=1e-6)]
+        )
+        self.downsample_layers.append(stem)
+        for i in range(3):
+            downsample_layer = tf.keras.Sequential([
+                    LayerNormalization(epsilon=1e-6),
+                    Conv2D(dims[i+1], kernel_size=2, strides=2, padding='same')]
+            )
+            self.downsample_layers.append(downsample_layer)
+
+        self.stages = [] # 4 feature resolution stages, each consisting of multiple residual blocks
+        dp_rates=[x for x in np.linspace(0, drop_path_rate, sum(depths))] 
+        cur = 0
+        for i in range(4):
+            stage = tf.keras.Sequential(
+                [Block(dim=dims[i], drop_path=dp_rates[cur + j], 
+                layer_scale_init_value=layer_scale_init_value, prefix=f'block{i}') for j in range(depths[i])]
+            )
+            self.stages.append(stage)
+            cur += depths[i]
+
+        if self.include_top:
+            self.avg = GlobalAveragePooling2D()
+            self.norm = LayerNormalization(epsilon=1e-6) # final norm layer
+            self.head = Dense(num_classes)
+        else:
+            self.avg = None
+            self.norm = None
+            self.head = None
+
+    def forward_features(self, x):
+        for i in range(4):
+            x = self.downsample_layers[i](x)
+            x = self.stages[i](x)
+        return  x
+
+    def call(self, x):
+        x = self.forward_features(x)
+        if self.include_top:
+            x = self.avg(x)
+            x = self.norm(x)
+            x = self.head(x)
+        return x
+
+
+model_urls = {
+    "convnext_tiny_1k": "https://dl.fbaipublicfiles.com/convnext/convnext_tiny_1k_224_ema.pth",
+    "convnext_small_1k": "https://dl.fbaipublicfiles.com/convnext/convnext_small_1k_224_ema.pth",
+    "convnext_base_1k": "https://dl.fbaipublicfiles.com/convnext/convnext_base_1k_224_ema.pth",
+    "convnext_large_1k": "https://dl.fbaipublicfiles.com/convnext/convnext_large_1k_224_ema.pth",
+    "convnext_base_22k": "https://dl.fbaipublicfiles.com/convnext/convnext_base_22k_224.pth",
+    "convnext_large_22k": "https://dl.fbaipublicfiles.com/convnext/convnext_large_22k_224.pth",
+    "convnext_xlarge_22k": "https://dl.fbaipublicfiles.com/convnext/convnext_xlarge_22k_224.pth",
+}
+
+model_configs = dict(
+    convnext_tiny=dict(
+        depths=[3, 3, 9, 3],
+        dims=[96, 192, 384, 768]
+    ),
+    convnext_small=dict(
+        depths=[3, 3, 27, 3],
+        dims=[96, 192, 384, 768]
+    ),
+    convnext_base=dict(
+        depths=[3, 3, 27, 3],
+        dims=[128, 256, 512, 1024]
+    ),
+    convnext_large=dict(
+        depths=[3, 3, 27, 3],
+        dims=[192, 384, 768, 1536]
+    ),
+    convnext_xlarge=dict(
+        depths=[3, 3, 27, 3],
+        dims=[256, 512, 1024, 2048]
+    ),
+)
+
+def convnext_tiny(pretrained=False, **kwargs):
+    model = ConvNeXt(depths=[3, 3, 9, 3], dims=[96, 192, 384, 768], **kwargs)
+    if pretrained:
+        url = model_urls['convnext_tiny_1k']
+        checkpoint = torch.hub.load_state_dict_from_url(url=url, map_location="cpu", check_hash=True)
+        model.load_state_dict(checkpoint["model"])
+    return model
+
+def convnext_small(pretrained=False, **kwargs):
+    model = ConvNeXt(depths=[3, 3, 27, 3], dims=[96, 192, 384, 768], **kwargs)
+    if pretrained:
+        url = model_urls['convnext_small_1k']
+        checkpoint = torch.hub.load_state_dict_from_url(url=url, map_location="cpu", check_hash=True)
+        model.load_state_dict(checkpoint["model"])
+    return model
+
+def convnext_base(pretrained=False, in_22k=False, **kwargs):
+    model = ConvNeXt(depths=[3, 3, 27, 3], dims=[128, 256, 512, 1024], **kwargs)
+    if pretrained:
+        url = model_urls['convnext_base_22k'] if in_22k else model_urls['convnext_base_1k']
+        checkpoint = torch.hub.load_state_dict_from_url(url=url, map_location="cpu", check_hash=True)
+        model.load_state_dict(checkpoint["model"])
+    return model
+
+def convnext_large(pretrained=False, in_22k=False, **kwargs):
+    model = ConvNeXt(depths=[3, 3, 27, 3], dims=[192, 384, 768, 1536], **kwargs)
+    if pretrained:
+        url = model_urls['convnext_large_22k'] if in_22k else model_urls['convnext_large_1k']
+        checkpoint = torch.hub.load_state_dict_from_url(url=url, map_location="cpu", check_hash=True)
+        model.load_state_dict(checkpoint["model"])
+    return model
+
+def convnext_xlarge(pretrained=False, in_22k=False, **kwargs):
+    model = ConvNeXt(depths=[3, 3, 27, 3], dims=[256, 512, 1024, 2048], **kwargs)
+    if pretrained:
+        url = model_urls['convnext_xlarge_22k'] if in_22k else model_urls['convnext_xlarge_1k']
+        checkpoint = torch.hub.load_state_dict_from_url(url=url, map_location="cpu", check_hash=True)
+        model.load_state_dict(checkpoint["model"])
+    return model
+
+def create_model(model_name='convnext_tiny_1k', input_shape=(224, 224), num_classes=1000, include_top=True, pretrained=True, use_tpu=False, **kwargs):
+    cfg = model_configs[model_name]
+    net = ConvNeXt(num_classes, cfg['depths'], cfg['dims'], include_top, **kwargs)
+    net(tf.keras.Input(shape=(*input_shape, 3)))
+    # if pretrained is True:
+    #     url = model_urls[model_name]
+    #     pretrained_ckpt = tf.keras.utils.get_file(
+    #         model_name, url, untar=True)
+
+    #     if tf.io.gfile.isdir(pretrained_ckpt):
+    #         pretrained_ckpt = f'{pretrained_ckpt}/{model_name}.ckpt'
+
+    #     if use_tpu:
+    #         load_locally = tf.saved_model.LoadOptions(
+    #             experimental_io_device='/job:localhost')
+    #         net.load_weights(pretrained_ckpt, options=load_locally)
+    #     else:
+    #         net.load_weights(pretrained_ckpt)
+
+    return net
